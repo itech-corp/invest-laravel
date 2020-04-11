8df1fddfee66d7271d73e167626b96788dfa724e
itech-corp <ibrahimmahamatmoussa@gmail.com>
2020-04-11 18:45:52 +0100

    Seems to work

--- resources/js/Index.js
+++ resources/js/Index.js
@@ -1,17 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import thunk from 'redux-thunk';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 
-import Main from './Router'; 
+import App from './src/App';
+import * as serviceWorker from './src/serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import authReducer from './src/store/reducers/auth';
+import payementReducer from './src/store/reducers/payement';
+import calculationReducer from './src/store/reducers/calculation';
 
-class Index extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <Route component={Main} />
-            </BrowserRouter>
-        );
-    }
-}
+import './src/index.css';
 
-ReactDOM.render(<Index />, document.getElementById('index'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const rootReducer = combineReducers({
+    auth: authReducer,
+    payement:payementReducer,
+    calculation:calculationReducer
+})
+
+const store = createStore(rootReducer, composeEnhancers(
+    applyMiddleware(thunk)
+));
+
+const app = (
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
+)
+
+
+ReactDOM.render(app, document.getElementById('index'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
